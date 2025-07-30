75bb89438633f2e361864ab956998d3dd61d70d3
Gowtham <avulagowtham76@gmail.com>
2025-07-30 21:12:53 +0530

    commit 156

---
+++ frontend/src/Pages/profile.jsx
@@ -0,0 +1,34 @@
+import React,{useEffect,useState} from 'react'
+import Service from '../../utils/http'
+import { Avatar, Container, Stack } from '@mantine/core';
+
+const service=new Service();
+const Profile=()=>{
+const [profileData,setProfileData]=useState(null);
+const getProfileData=async()=>{
+    let data=await service.get('user/me');
+    setProfileData(data);
+    console.log(data);
+}
+useEffect(()=>{
+    getProfileData();
+},[])
+return (
+    <Container size={"sm"}>
+    <Stack h={300}
+      bg="var(--mantine-color-body)"
+      align="center"
+      justify="center"
+      gap="lg">
+        <Avatar size={"lg"} radius={"xl"} src={profileData?.avatar}/>
+        <Text id="dd"> {profileData?.name}</Text>
+        <Text>{profileData?.email}</Text>
+        <Text id="gg"> <b>User Id :</b> {profileData?._id}</Text>
+        <Text id="hh"><b>Created Account At :</b>{profileData?.createdAt}</Text>
+    </Stack> 
+    </Container>  
+    
+  )
+}
+
+export default  Profile
